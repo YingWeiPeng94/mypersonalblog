b2356a2729e6c778ce73a3f57ca4e534ea905f30
YingWeiPeng94 <1727142928@qq.com>
2020-02-28 14:05:56 +0800

    个人博客

--- dao/dbutil.js
+++ dao/dbutil.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 function createConnection(){
     var connection = mysql.createConnection({
-        host:"127.0.0.1",
+        host:"192.168.1.104",
         port:"3306",
         user:"root",
         password:"YING65123478",
